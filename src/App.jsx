776dbffc1dfc0f600fe21632c0b3c873b63e6481
NOUREDDINE <noureddine.web3dev@gmail.com>
2025-12-13 02:31:12 +0100

    Fetching Data functions created + menu updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,14 @@ import Cart from './features/cart/Cart';
 import CreateOrder from './features/user/CreateOrder';
 import Order from './features/order/Order';
 import AppLayout from './ui/AppLayout';
+import { loader as menuLoader } from './features/menu/Menu';
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
     children: [
       { path: '/', element: <Home /> },
-      { path: '/menu', element: <Menu /> },
+      { path: '/menu', element: <Menu />, loader: menuLoader },
       { path: '/cart', element: <Cart /> },
       { path: '/order/new', element: <CreateOrder /> },
       { path: 'order/:orderId', element: <Order /> },
